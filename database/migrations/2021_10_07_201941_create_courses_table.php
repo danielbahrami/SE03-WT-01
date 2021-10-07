518b6887ef3a925c269174d659a892bbe98c0c05
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2021-10-07 22:51:37 +0200

    Courses migration

---
+++ database/migrations/2021_10_07_201941_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->string('name');
+            $table->string('code');
+            $table->integer('ects');
+            $table->text('description');
+            $table->integer('department_id')->unsigned();
+            $table->foreign('department_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
