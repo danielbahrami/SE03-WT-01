a7fe360b958d2e2dcef58028383c2225f50c9f71
Daniel Bahrami <daniel@bahrami.dk>
2021-10-11 01:41:34 +0200

    Create department form

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -11,7 +11,23 @@ class DepartmentController extends Controller
 
     public function createDepartmentForm()
     {
-        return 'Create department form';
+        return <<<HTML
+<html>
+<body>
+<h1>Create department</h1>
+<form method="post">
+<label for="name">Name:</label><br>
+<input type="text" id="name" name="name"><br>
+<label for="code">Code:</label><br>
+<input type="text" id="code" name="code"><br>
+<label for="description">Description:</label><br>
+<textarea id="description" cols="50" rows="10"></textarea><br>
+<input name="Submit" type="submit" value="Create department">
+</form>
+</body>
+</html>
+HTML;
+
     }
 
     public function createDepartment()
