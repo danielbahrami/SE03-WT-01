ad9c27ae2c0b0458f54b5f7a8c3d60b13df143ad
Daniel Bahrami <daniel@bahrami.dk>
2021-10-11 00:32:38 +0200

    Updated welcome page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,7 +8,14 @@ class MainController extends Controller
     {
         return <<<HTML
 <html>
-<h1>Welcome to itsLearning!!!</h1>
+<body><h1>Welcome to itsLearning</h1>
+<br>
+<h2>
+ <a href="/departments">Departments</a>
+ <br>
+ <a href="/courses">Courses</a>
+</h2>
+</body>
 </html>
 HTML;
     }
