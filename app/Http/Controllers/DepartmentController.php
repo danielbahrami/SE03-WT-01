e43e6f7a59f557f3c422bc5afb765346f7e9e7b9
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2021-10-11 04:26:05 +0200

    Create department list
    Show link mangler pga task 4

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -6,7 +6,67 @@ class DepartmentController extends Controller
 {
     public function listDepartment()
     {
-        return 'List department';
+        return <<<HTML
+<html>
+<head>
+<style>
+.department {
+font-family: arial, sans-serif;
+border-collapse: collapse;
+width: 100%;
+}
+.code{
+border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+.name {
+border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+.courses {
+border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+.show {
+border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+.success{
+
+}
+</style>
+</head>
+<body>
+
+<h2>List over departments</h2>
+
+<table class="department">
+  <tr>
+    <th class="code">Code</th>
+    <th class="name">Name</th>
+    <th class="courses">Courses</th>
+    <th class="show">Show</th>
+  </tr>
+  <tr>
+    <td class="code">456</td>
+    <td class="name">IT Department</td>
+    <td class="courses">3 courses</td>
+    <td class="show"><a href=http://127.0.0.1:8000/departments/{department}>Show</a></td>
+  </tr>
+</table>
+<div>
+<h2>Create department</h2>
+<p> To create a new department please press <a href=http://127.0.0.1:8000/departments/create>her</a> </p>
+
+</div>
+</body>
+
+HTML;
+
     }
 
     public function createDepartmentForm()
