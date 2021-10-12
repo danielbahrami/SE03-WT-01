46056366e452f64846a5902830b3ce1492b59216
Daniel Bahrami <daniel@bahrami.dk>
2021-10-13 00:55:57 +0200

    Added more line breaks

--- resources/views/createDepartment.blade.php
+++ resources/views/createDepartment.blade.php
@@ -4,11 +4,11 @@
 <form action="create" method="POST">
     @csrf
     <label for="name">Name:</label><br>
-    <input type="text" id="name" name="name" placeholder="Enter name"><br>
+    <input type="text" id="name" name="name" placeholder="Enter name"><br><br>
     <label for="code">Code:</label><br>
-    <input type="text" id="code" name="code" placeholder="Enter code"><br>
+    <input type="text" id="code" name="code" placeholder="Enter code"><br><br>
     <label for="description">Description:</label><br>
-    <input type="text" id="description" name="description" placeholder="Enter description"><br>
+    <input type="text" id="description" name="description" placeholder="Enter description"><br><br>
     <button class="submit" type="submit">Create department</button>
 </form>
 </body>
