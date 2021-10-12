f0d7085cf239e17f987bfc931c8e7413a43e44bd
Daniel Bahrami <daniel@bahrami.dk>
2021-10-13 00:56:35 +0200

    Small changes

---
+++ app/Http/Controllers/DepartmentsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Department;
+
+class DepartmentsController extends Controller
+{
+    public function index()
+    {
+        $data = Department::all();
+        return view('departments', ['departments' => $data]);
+    }
+
+    public function createDepartment(Request $request)
+    {
+        $deparment = new Department();
+        $deparment->name = $request->name;
+        $deparment->code = $request->code;
+        $deparment->description = $request->description;
+        $deparment->save();
+        return redirect('/departments')->with('message', 'Success!');
+    }
+
+    public function showDepartment(Department $department)
+    {
+        return view($this->showDepartment(), ['id' => $department]);
+    }
+
+}
