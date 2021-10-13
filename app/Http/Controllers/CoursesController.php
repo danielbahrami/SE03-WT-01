c9f2364359233a476ae05195d8462fb914e507c1
Daniel Bahrami <daniel@bahrami.dk>
2021-10-14 00:35:19 +0200

    Courses controller

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -1,11 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Course;
 
 class CoursesController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        $data = Course::all();
+        return view('courses', ['courses' => $data]);
+    }
 
+    public function createCourse(Request $request)
+    {
+        $course = new Course();
+        $course->name = $request->name;
+        $course->code = $request->code;
+        $course->description = $request->description;
+        $course->save();
+        return redirect('/courses')->with('message', 'Success!');
+    }
+
+    public function showCourse($id)
+    {
+        return Department::find($id);
     }
 
 }
