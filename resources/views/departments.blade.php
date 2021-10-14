e028be8f4f7bd90e6de746adef486a9a6e6640bd
Daniel Bahrami <daniel@bahrami.dk>
2021-10-14 21:29:21 +0200

    Update departments.blade.php

--- resources/views/departments.blade.php
+++ resources/views/departments.blade.php
@@ -1,4 +1,34 @@
 <html>
+<body>
+
+<h1>List of departments</h1>
+
+<table class="department">
+    <tr>
+        <th class="code">Code</th>
+        <th class="name">Name</th>
+        <th class="courses">Courses</th>
+        <th class="show">Show</th>
+    </tr>
+    @foreach($departments as $department)
+        <tr>
+            <td class="code">{{$department['code']}}</td>
+            <td class="name">{{$department['name']}}</td>
+            <td class="courses">0</td>
+            <td class="show"><a href={{url('/departments', [$department ->id])}}>Show</a></td>
+        </tr>
+    @endforeach
+</table>
+
+<a class="new" href=/departments/create><h2>Create new department</h2></a>
+
+@if(session('message'))
+    <div class="success-message">{{session('message')}}</div>
+@endif
+
+</body>
+</html>
+
 <style>
     .department {
         font-family: arial, sans-serif;
@@ -32,34 +62,4 @@
     .success-message {
         font-family: arial, sans-serif;
     }
-
 </style>
-<body>
-<h1>List of departments</h1>
-
-@if(session('message'))
-    <div class="success-message">{{session('message')}}</div>
-@endif
-
-
-<table class="department">
-    <tr>
-        <th class="code">Code</th>
-        <th class="name">Name</th>
-        <th class="courses">Courses</th>
-        <th class="show">Show</th>
-    </tr>
-    @foreach($departments as $department)
-        <tr>
-            <td class="code">{{$department['code']}}</td>
-            <td class="name">{{$department['name']}}</td>
-            <td class="courses">0</td>
-            <td class="show"><a href={{url('/departments', [$department ->id])}}>Show</a></td>
-        </tr>
-    @endforeach
-
-</table>
-
-<a class="new" href=/departments/create><h2>Create new department</h2></a>
-</div>
-</body>
