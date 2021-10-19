74517c25452011eeb3f20b187fd58783812b1b75
Daniel Bahrami <daniel@bahrami.dk>
2021-10-19 15:18:46 +0200

    ShowDepartment

--- resources/views/showDepartment.blade.php
+++ resources/views/showDepartment.blade.php
@@ -1,8 +1,8 @@
-<h1>Show Department</h1>
-<p>{{$department->code}}</p>
-<p>{{$department->name}}</p>
-<p>{{$department->description}}</p>
-
+<h1>Department</h1>
+<h2>Code:</h2>
+<p>{{$department -> code}}</p>
+<h2>Name:</h2>
+<p>{{$department -> name}}</p>
+<h2>Description:</h2>
+<p>{{$department -> description}}</p>
 <h1>Courses</h1>
-
-<table
