7f4bf03e226658a36ba5ecdae02abd9b1c24d2cc
Daniel Bahrami <daniel@bahrami.dk>
2021-10-20 13:14:24 +0200

    Reformat code

--- app/Http/Controllers/DepartmentsController.php
+++ app/Http/Controllers/DepartmentsController.php
@@ -15,36 +15,36 @@ class DepartmentsController extends Controller
 
     public function createDepartment(Request $request) {
         $department = new Department();
-        $department->code = $request->code;
-        $department->name = $request->name;
-        $department->description = $request->description;
-        $department->save();
-        return redirect('/departments')->with('message', 'Department "' . $department->code . '" created successfully');
+        $department -> code = $request -> code;
+        $department -> name = $request -> name;
+        $department -> description = $request -> description;
+        $department -> save();
+        return redirect('/departments') -> with('message', 'Department "' . $department -> code . '" created successfully');
     }
 
     public function showDepartment($id) {
         $department = Department::find($id);
-        $courses = $department->courses;
-       return view('showDepartment', compact('department','courses'));
+        $courses = $department -> courses;
+       return view('showDepartment', compact('department', 'courses'));
     }
 
     public function editDepartment($id) {
         $department = Department::find($id);
-        return view('editDepartment',['department'=>$department]);
+        return view('editDepartment', ['department' => $department]);
     }
 
     public function updateDepartment(Request $request) {
-        $department = Department::find($request->id);
+        $department = Department::find($request -> id);
         $department -> name = $request -> name;
         $department -> code = $request -> code;
         $department -> description = $request -> description;
         $department -> save();
-        return redirect('/departments')->with('message', 'Department "' . $department->code . '" updated successfully');
+        return redirect('/departments') -> with('message', 'Department "' . $department -> code . '" updated successfully');
     }
 
     public function deleteDepartment($id) {
         $department = Department::find($id);
-        $department->delete();
-        return redirect('/departments')->with('message', 'Department "' . $department->code . '" successfully removed');
+        $department -> delete();
+        return redirect('/departments') -> with('message', 'Department "' . $department -> code . '" successfully removed');
     }
 }
