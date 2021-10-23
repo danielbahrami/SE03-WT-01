72be907d40fd390707caa555251f6598e60b3fb7
Daniel Bahrami <daniel@bahrami.dk>
2021-10-23 21:46:14 +0200

    Edit course

--- resources/views/editCourse.blade.php
+++ resources/views/editCourse.blade.php
@@ -4,16 +4,31 @@
 <form action="/editCourse" method="POST">
     @csrf
     <input type="hidden" name="id" value="{{$course['id']}}">
-    <label for="department">Department:</label><br>
-    <!--<input type="" id="department" name="department" value="{{$course['department_id']}}">-->
-    <label for="name">Name:</label><br>
-    <input type="text" id="name" name="name" value="{{$course['name']}}"><br><br>
-    <label for="code">Code:</label><br>
-    <input type="text" id="code" name="code" value="{{$course['code']}}"><br><br>
-    <label for="ects">ECTS:</label><br>
-    <input type="number" id="ects" name="ects" value="{{$course['ects']}}"><br><br>
-    <label for="description">Description:</label><br>
-    <input type="text" id="description" name="description" value="{{$course['description']}}"><br><br>
+    <label for="department_id">Department:</label>
+    <br>
+    <select name="department_id" id="department_id">
+        <option selected="{{$course['department_id']}}"></option>
+        @foreach($department as $department)
+            <option value="{{$department -> id}}">{{$department -> name}}</option>
+        @endforeach
+    </select>
+    <br><br>
+    <label for="name">Name:</label>
+    <br>
+    <input type="text" id="name" name="name" value="{{$course['name']}}">
+    <br><br>
+    <label for="code">Code:</label>
+    <br>
+    <input type="text" id="code" name="code" value="{{$course['code']}}">
+    <br><br>
+    <label for="ects">ECTS:</label>
+    <br>
+    <input type="number" id="ects" name="ects" value="{{$course['ects']}}">
+    <br><br>
+    <label for="description">Description:</label>
+    <br>
+    <input type="text" id="description" name="description" value="{{$course['description']}}">
+    <br><br><br>
     <button class="submit" type="submit">Update course</button>
 </form>
 </body>
