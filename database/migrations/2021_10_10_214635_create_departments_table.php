b87f85a0b040f0306da8a06a729d8bebd04b33b9
Daniel Bahrami <daniel@bahrami.dk>
2021-10-27 13:13:53 +0200

    Clean up migrations

--- database/migrations/2021_10_10_214635_create_departments_table.php
+++ database/migrations/2021_10_10_214635_create_departments_table.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDepartmentsTable extends Migration
-{
-    public function up()
-    {
+class CreateDepartmentsTable extends Migration {
+
+    public function up() {
         Schema::create('departments', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
-            $table->string('code')->unique();
-            $table->string('description');
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table -> id();
+            $table -> string('name') -> unique();
+            $table -> string('code') -> unique();
+            $table -> string('description');
+            $table -> timestamp('created_at') ->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table -> timestamp('updated_at') ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('departments');
     }
 }
